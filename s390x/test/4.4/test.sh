86a33ff2e753c9e7497fc2ad5c48a19d4586c739
lcarcaramo <72973973+lcarcaramo@users.noreply.github.com>
2020-11-11 12:57:43 -0500

    Fix syntax.

--- s390x/test/4.4/test.sh
+++ s390x/test/4.4/test.sh
@@ -50,7 +50,7 @@ suite_end () {
 suite_start
         print_test_case "It can start a new mongodb container.:"
                 docker run --rm --name mongodb-container-runs -d quay.io/ibmz/mongo:4.4.1
-                $DATABASE_FOUND=$(docker exec mongodb-container-runs mongo --eval db | grep test)
+                export DATABASE_FOUND=$(docker exec mongodb-container-runs mongo --eval db | grep test)
                 print_success "Success! The database \"$DATABASE_FOUND\" was found in the started quay.io/ibmz/mongo:4.4.1 container."
                 print_success "This is an indication that MongoDB started successfully and was able to initialize the \"$DATABASE_FOUND\" database."
                 print_success "Terminating..."
